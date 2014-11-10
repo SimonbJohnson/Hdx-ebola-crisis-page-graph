fadef814b54137c30b6535b52fd39d8f6fdc728e
SimonbJohnson <simonbjohnson@gmail.com>
2014-11-10 20:07:19 +0000

    Shows breakdown on move over

--- js/ebola_crisis_page_graph.js
+++ js/ebola_crisis_page_graph.js
@@ -11,68 +11,101 @@ $.ajax({
   url: 'https://data.hdx.rwlabs.org/api/3/action/datastore_search_sql',
   data: data,
   success: function(data) {
-      console.log(data);
-      var prcoessedData = processData(data.result.records);
-      generateLineChart('#ebola_graph',prcoessedData);
+      var processedData = processData(data.result.records);
+      generateLineChart('#ebola_graph',processedData);
   }
 });
 
 function processData(dataIn){
-    var deathsData = [];
-    var casesData=[];
-    var deathsFirstLine = true;
-    var casesFirstLine = true;
+    var data = [];
+    var firstLine = true;
     dataIn.forEach(function(e){
+        if(firstLine || e['Date']!=data[data.length-1]['date']){
+            data.push({
+                'date':e['Date'],
+                'deaths':{
+                'total':0,
+                'Guinea':0,
+                'Liberia':0,
+                'Sierra Leone':0,
+                'other':0
+                },
+                'cases':{
+                'total':0,
+                'Guinea':0,
+                'Liberia':0,
+                'Sierra Leone':0,
+                'other':0
+                }
+            });
+            firstLine=false;
+        }
         if(e.Indicator=='Cumulative number of confirmed, probable and suspected Ebola deaths'){
-            if(deathsFirstLine || e['Date']!=deathsData[deathsData.length-1]['date']){
-                deathsData.push({
-                    'date':e['Date'],
-                    'total':0,
-                    'Guinea':0,
-                    'Liberia':0,
-                    'Sierra Leone':0,
-                    'other':0
-                });
-                deathsFirstLine=false;
-            }
-            deathsData[deathsData.length-1]['total']+=e['value'];
+            data[data.length-1]['deaths']['total']+=e['value'];
             if(e['Country']=='Guinea'||e['Country']=='Liberia'||e['Country']=='Sierra Leone'){
-                deathsData[deathsData.length-1][e['Country']]=e['value'];
+                data[data.length-1]['deaths'][e['Country']]=e['value'];
             } else {
-                deathsData[deathsData.length-1]['other']+=e['value'];
+                data[data.length-1]['deaths']['other']+=e['value'];
             }
         } else {
-            if(casesFirstLine || e['Date']!=casesData[casesData.length-1]['date']){
-               casesData.push({
-                    'date':e.Date,
-                    'total':0,
-                    'Guinea':0,
-                    'Liberia':0,
-                    'Sierra Leone':0,
-                    'other':0
-                });
-                casesFirstLine=false;
-            }
-            casesData[casesData.length-1]['total']+=e['value'];
+            data[data.length-1]['cases']['total']+=e['value'];
             if(e['Country']=='Guinea'||e['Country']=='Liberia'||e['Country']=='Sierra Leone'){
-                casesData[casesData.length-1][e['Country']]=e['value'];
+                data[data.length-1]['cases'][e['Country']]=e['value'];
             } else {
-                casesData[casesData.length-1]['other']+=e['value'];
+                data[data.length-1]['cases']['other']+=e['value'];
             }            
         }
     });
-    return {cases:casesData,deaths:deathsData};
+    return data;
 };
 
 function generateLineChart(id,data){
-    console.log(data);
-    data.deaths.forEach(function(e){
+    data.forEach(function(e){
         e.date = new Date(e.date);
     });
-    data.cases.forEach(function(e){
-        e.date = new Date(e.date);
-    });
-    var margin = {top: 20, right: 20, bottom: 25, left: 50},
+    
+    var varNames = d3.keys(data[0].deaths).filter(function (key) { return key !== 'total';});;
+    
+    var seriesDeathArr = [], series = {};
+        varNames.forEach(function (name) {
+          series[name] = {name: name, values:[]};
+          seriesDeathArr.push(series[name]);
+        });
+        data.forEach(function (d) {
+          varNames.map(function (name) {
+            series[name].values.push({label: d.date, value: +d.deaths[name]});
+          });
+        });
+        
+    var seriesDeathArr = [], series = {};
+        varNames.forEach(function (name) {
+          series[name] = {name: name, values:[]};
+          seriesDeathArr.push(series[name]);
+        });
+        data.forEach(function (d) {
+          varNames.map(function (name) {
+            series[name].values.push({label: d.date, value: +d.deaths[name]});
+          });
+        });        
+        
+    var deathColor = d3.scale.ordinal()
+          .range(["#B71C1C","#E53935","#EF9A9A","#FFEBEE"]);        
+  
+    var seriesCaseArr = [], series = {};
+        varNames.forEach(function (name) {
+          series[name] = {name: name, values:[]};
+          seriesCaseArr.push(series[name]);
+        });
+        data.forEach(function (d) {
+          varNames.map(function (name) {
+            series[name].values.push({label: d.date, value: +d.cases[name]});
+          });
+        });        
+        console.log(seriesCaseArr);
+    var caseColor = d3.scale.ordinal()
+          .range(["#1A237E","#3949AB","#7986CB","#E8EAF6"]); 
+
+    var margin = {top: 20, right: 120, bottom: 25, left: 50},
         width = $(id).width() - margin.left - margin.right,
         height = $(id).height() - margin.top - margin.bottom;
 
@@ -82,9 +115,9 @@ function generateLineChart(id,data){
     var y = d3.scale.linear()
             .range([height, 0]);
     
-    x.domain(d3.extent(data.deaths, function(d) { 
+    x.domain(d3.extent(data, function(d) { 
         return d.date; }));
-    y.domain([0,d3.max(data.cases, function(d) { return d.total; })]);    
+    y.domain([0,d3.max(data, function(d) { return d.cases.total; })]);    
 
     var xAxis = d3.svg.axis()
         .scale(x)
@@ -94,13 +127,41 @@ function generateLineChart(id,data){
         .scale(y)
         .orient("left");
 
-    var line = d3.svg.line()
+    var deathLine = d3.svg.line()
         .x(function(d) {
-            console.log(d.date);
             return x(d.date);
         })
-        .y(function(d) { return y(d.total); });
+        .y(function(d) { return y(d.deaths.total); });
 
+    var caseLine = d3.svg.line()
+        .x(function(d) {
+            return x(d.date);
+        })
+        .y(function(d) { return y(d.cases.total); });
+
+    var deathArea = d3.svg.area()
+        .x(function(d) { return x(d.date); })
+        .y0(function(d) { return y(0); })
+        .y1(function(d) { return y(d.deaths.total); });
+
+    var caseArea = d3.svg.area()
+        .x(function(d) { return x(d.date); })
+        .y0(function(d) { return y(d.deaths.total); })
+        .y1(function(d) { return y(d.cases.total); });
+
+    var stack = d3.layout.stack()
+        .offset("zero")
+        .values(function (d) { return d.values; })
+        .x(function (d) { return x(d.label); })
+        .y(function (d) { return d.value; });
+
+    var area = d3.svg.area()
+        .x(function (d) { return x(d.label); })
+        .y0(function (d) { return y(d.y0); })
+        .y1(function (d) { return y(d.y0 + d.y); });
+  
+    stack(seriesDeathArr);
+    stack(seriesCaseArr);
     var svg = d3.select(id).append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
@@ -121,21 +182,99 @@ function generateLineChart(id,data){
         .attr("y", 6)
         .attr("dy", ".71em")
         .style("text-anchor", "end")
-        .text("Cases");
+        .text("Persons");
+
+    svg.append("g")
+        .append("text")
+        .attr("x",width+10)
+        .attr("y", y(data[data.length-1].cases.total))
+        .attr("dy", ".2em")
+        .style("text-anchor", "start")
+        .text("Cases ("+data[data.length-1].cases.total+")")
+        .attr("class","linelabels");
+
+    svg.append("g")
+        .append("text")
+        .attr("x",width+10)
+        .attr("y", y(data[data.length-1].deaths.total))
+        .attr("dy", ".2em")
+        .style("text-anchor", "start")
+        .text("Deaths ("+data[data.length-1].deaths.total+")")
+        .attr("class","linelabels");
 
     svg.append("path")
-        .datum(data.cases)
+        .datum(data)
         .attr("class", "line")
-        .attr("d", line)
+        .attr("d", caseLine)
         .attr("fill","none")
         .attr("stroke","steelblue")
         .attr("stroke-width","1px");
 
     svg.append("path")
-        .datum(data.deaths)
-        .attr("class", "line2")
-        .attr("d", line)
+        .datum(data)
+        .attr("class", "line")
+        .attr("d", deathLine)
         .attr("fill","none")
         .attr("stroke","red")
         .attr("stroke-width","1px");
+
+    var selection = svg.selectAll(".seriesdeath")
+          .data(seriesDeathArr)
+          .enter().append("g")
+            .attr("class", "series");
+
+        selection.append("path")
+          .attr("class", "deathPath")
+          .attr("d", function (d) { return area(d.values); })
+          .style("fill", function (d) { return deathColor(d.name); })
+          .style("stroke", function (d) { return deathColor(d.name); })
+          .attr("opacity",0);
+
+      var selection = svg.selectAll(".seriescases")
+          .data(seriesCaseArr)
+          .enter().append("g")
+            .attr("class", "series");
+
+        selection.append("path")
+            .attr("class", "casePath")
+            .attr("d", function (d) { return area(d.values); })
+            .style("fill", function (d) { return caseColor(d.name); })
+            .style("stroke", function (d) { return caseColor(d.name); })
+            .attr("opacity",0);
+        
+        selection.append("text")
+            .attr("x", width+10)
+            .attr("y", 10)
+            .attr("dy", ".2em")
+            .style("text-anchor", "start")
+            .text(function(d){;return d.name;})
+            .attr("opacity",1);
+
+    svg.append("path")
+        .datum(data)
+        .attr("class", "area")
+        .attr("d", deathArea)
+        .attr("opacity",0)
+        .on("mouseover",function(){
+            d3.selectAll(".deathPath").transition().duration(500).attr("opacity",0.5);
+            d3.selectAll(".linelabels").transition().duration(500).attr("opacity",0);
+        })
+        .on("mouseout",function(){
+            d3.selectAll(".deathPath").transition().duration(500).attr("opacity",0);
+            d3.selectAll(".linelabels").transition().duration(500).attr("opacity",1);
+        });        
+
+    svg.append("path")
+        .datum(data)
+        .attr("class", "area")
+        .attr("d", caseArea)
+        .attr("opacity",0)
+        .on("mouseover",function(){
+            d3.selectAll(".casePath").transition().duration(500).attr("opacity",0.5);
+            d3.selectAll(".linelabels").transition().duration(500).attr("opacity",0);
+        })
+        .on("mouseout",function(){
+            d3.selectAll(".casePath").transition().duration(500).attr("opacity",0);
+            d3.selectAll(".linelabels").transition().duration(500).attr("opacity",1);
+        })
 }
